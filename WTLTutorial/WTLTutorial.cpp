699efcdf44a7b362d11af20bf1ebf75925a858ed
kyaru <kyaruny@gmail.com>
2023-03-09 06:05:52 +0800

    使用WTL

--- WTLTutorial/WTLTutorial.cpp
+++ WTLTutorial/WTLTutorial.cpp
@@ -4,98 +4,45 @@
 #include "framework.h"
 #include "WTLTutorial.h"
 
-template<typename Derived>
-class BaseWindow {
+CAppModule _Module;
+
+class MyWindow : public CWindowImpl<MyWindow> {
 public:
-    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-        if (uMsg == WM_CREATE) {
-            BaseWindow* self = nullptr;
-            auto cs = reinterpret_cast<CREATESTRUCT*>(lParam);
-            self = reinterpret_cast<BaseWindow*>(cs->lpCreateParams);
-            SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)self);
-            self->_hwnd = hwnd;
-            return 0;
-        }
-
-        Derived* derived = reinterpret_cast<Derived*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
-        if (derived) {
-            return derived->HandleMessage(uMsg, wParam, lParam);
-        }
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
-    }
-
-    BOOL Create(PCWSTR lpWindowName, DWORD dwStyle, DWORD dwExStyle = 0,
-        int x = CW_USEDEFAULT, int y = CW_USEDEFAULT, int nWidth = CW_USEDEFAULT, int nHeight = CW_USEDEFAULT,
-        HWND hWndParent = 0, HMENU hMenu = 0
-    ){
-        Derived* underlying = static_cast<Derived*>(this);
-
-        WNDCLASS wc = { 0 };
-        wc.lpfnWndProc = BaseWindow<Derived>::WindowProc;
-        wc.hInstance = GetModuleHandle(NULL);
-        wc.lpszClassName = underlying->ClassName();
-        RegisterClass(&wc);
-
-        _hwnd = CreateWindowEx(
-            dwExStyle, underlying->ClassName(), lpWindowName, dwStyle, x, y,
-            nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
-        );
-
-        return (_hwnd ? TRUE : FALSE);
-    }
-
-    HWND Window() const {
-        return _hwnd;
-    }
-protected:
-    HWND _hwnd;
-};
-
-class MainWindow: public BaseWindow<MainWindow> {
-public:
-    LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) {
-        switch (uMsg)
-        {
-        case WM_SIZE:
-        {
-            OnSize(lParam);
-            return 0;
-        }
-        case WM_PAINT:
-        {
-            OnPaint();
-            return 0;
-        }
-        case WM_CLOSE:
-            if (MessageBox(_hwnd, L"确认退出?", L"Windows程序", MB_OKCANCEL) == IDOK) {
-                DestroyWindow(_hwnd);
-            }
-            return 0;  // 忽略该消息
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-        default:
-            break;
-        }
-        return DefWindowProc(_hwnd, uMsg, wParam, lParam);
-    }
-
-    const wchar_t* ClassName() const {
-        return _className;
-    }
+    DECLARE_WND_CLASS(_className);
+    BEGIN_MSG_MAP(MyWindow)
+        MESSAGE_HANDLER(WM_DESTROY, OnDestory)
+        MESSAGE_HANDLER(WM_SIZE, OnSize)
+        MESSAGE_HANDLER(WM_CLOSE, OnClose)
+        MESSAGE_HANDLER(WM_PAINT, OnPaint)
+    END_MSG_MAP()
 private:
-    void OnSize(LPARAM lParam) {
+    LRESULT OnDestory(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& handled) {
+        OutputDebugString(L"Exiting...\n");
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    LRESULT OnClose(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& handled)
+    {
+        if (MessageBox(L"确定退出?", L"WTL", MB_OKCANCEL) == IDOK) {
+            DestroyWindow();
+        }
+        return 0;
+    }
+
+    LRESULT OnSize(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& handled) {
         ++this->_nResize;
         wchar_t buf[1024];
         int width = LOWORD(lParam);
         int height = HIWORD(lParam);
         swprintf_s(buf, 1024, L"第%d次resize窗口: (%d, %d)\n", this->_nResize, width, height);
         OutputDebugString(buf);
+        return 0;
     }
 
-    void OnPaint() {
+    LRESULT OnPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& handled) {
         PAINTSTRUCT ps;
-        auto hdc = BeginPaint(_hwnd, &ps);  // 获取要更新绘制区域的信息
+        auto hdc = BeginPaint(&ps);  // 获取要更新绘制区域的信息
         auto rc = ps.rcPaint;
         wchar_t buf[1024];
         swprintf_s(buf, 1024,
@@ -105,30 +52,34 @@ private:
         // ALL PAINTING STUFF
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 
-        EndPaint(_hwnd, &ps);  // 清空Update Region, 不再发WM_PAINT消息
+        EndPaint(&ps);  // 清空Update Region, 不再发WM_PAINT消息
+        return 0;
     }
 
-    int _nResize;
-    const wchar_t* const _className = L"Sample Window Class";
+    int _nResize = 0;
+    static const wchar_t* const _className;
 };
+
+const wchar_t* const MyWindow::_className = L"Sample Window Class";
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
-    MainWindow *mw = new MainWindow();
+    CMessageLoop loop;
+    _Module.Init(NULL, hInstance);
+    _Module.AddMessageLoop(&loop);
 
-    if (!mw->Create(L"Window编程", WS_OVERLAPPEDWINDOW)) {
-        return 0;
-    }
-    ShowWindow(mw->Window(), nCmdShow);
+    MyWindow mw;
+    mw.Create(NULL, CWindow::rcDefault, L"WTL窗口", WS_OVERLAPPEDWINDOW);
+    mw.ShowWindow(nCmdShow);
+    mw.UpdateWindow();
 
-    MSG msg{};
-    while (GetMessage(&msg, NULL, 0, 0) > 0) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+    loop.Run();
+
+    _Module.RemoveMessageLoop();
+    _Module.Term();
 
     return 0;
 }
